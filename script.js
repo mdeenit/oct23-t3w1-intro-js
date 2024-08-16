5ad1647f81e173ef8490023a449d82bf53d03b38
JarupongMin <129025834+mdeenit@users.noreply.github.com>
2024-08-16 19:33:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,149 +1,188 @@
-let exampleEmptyVariable = null;
-let anotherEmptyVariable = undefined;
+function exampleStuff(){
+	let exampleEmptyVariable = null;
+	let anotherEmptyVariable = undefined;
+	
+	console.log(exampleEmptyVariable, anotherEmptyVariable);
+	
+	let message = "hello from the script.js file!";
+	
+	console.log(message);
+	
+	// // no keyword = declared with var
+	// world = "Earth";
+	
+	// console.log(`Hello ${world}, I am Alex!`);
+	
+	// console.log("Hello " + world + ", I am Alex!");
+	
+	// let world = "Mars";
+	
+	// console.log(`Hello ${world}, I am Alex!`);
+	
+	// {
+	
+	// 	//let world = "Mars";
+	
+	// 	console.log(`Hello ${world}, I am Alex!`);
+	
+	// }
+	
+	
+	
+	
+	
+	
+	
+	// List in Python is Array in JS
+	
+	let favouriteFruits = [
+		"mango",
+		"banana",
+		"grapes",
+		"passionfruit", 
+		"pear",
+		"strawberry",
+		"watermelon",
+		"lychee",
+		"cherry",
+		"blueberry",
+		"tomato",
+		"papaya",
+		"dragon fruit",
+		"kiwi"
+	];
+	
+	console.log(favouriteFruits[12]);
+	
+	console.log("The class' favourite fruits are: " + favouriteFruits);
+	
+	
+	// favouriteFruits.forEach(fruit => {
+	// 	console.log("Someone in the class loves " + fruit);
+	// });
+	
+	for (fruitIndex in favouriteFruits) {
+		console.log("Someone in the class loves " + favouriteFruits[fruitIndex]);
+	}
+	
+	
+	console.log(Array.isArray(favouriteFruits));
+	
+	
+	// Array.prototype.sort()
+	let sortedFavouriteFruits = favouriteFruits.sort();
+	
+	console.log(sortedFavouriteFruits);
+	
+	let someNewArray = new Array(10);
+	
+	someNewArray.push("mango");
+	
+	console.log(someNewArray);
 
-console.log(exampleEmptyVariable, anotherEmptyVariable);
+	
+	
+	
+	
+	// Dictionary in Python is Object in JS
+	
+	let alex = {
+		name: "alex",
+		"codingLanguages": ["C#", "JavaScript", "HTML", "CSS"],
+		hobbies: [
+			{
+				"name": "gardening",
+				"skill": 10,
+			},
+			{
+				"name": "gaming",
+				"favouriteGames": ["pokemon", "halo"],
+				"skill": 9001,
+			}
+		],
+	};
 
-let message = "hello from the script.js file!";
-
-console.log(message);
-
-// // no keyword = declared with var
-// world = "Earth";
-
-// console.log(`Hello ${world}, I am Alex!`);
-
-// console.log("Hello " + world + ", I am Alex!");
-
-// let world = "Mars";
-
-// console.log(`Hello ${world}, I am Alex!`);
-
-// {
-
-// 	//let world = "Mars";
-
-// 	console.log(`Hello ${world}, I am Alex!`);
-
-// }
-
-
-
-
-
-
-
-// List in Python is Array in JS
-
-let favouriteFruits = [
-	"mango",
-	"banana",
-	"grapes",
-	"passionfruit",
-	"pear",
-	"strawberry",
-	"watermelon",
-	"lychee",
-	"cherry",
-	"blueberry",
-	"tomato",
-	"papaya",
-	"dragon fruit",
-	"kiwi"
-];
-
-console.log(favouriteFruits[12]);
-
-console.log("The class' favourite fruits are: " + favouriteFruits);
-
-
-// favouriteFruits.forEach(fruit => {
-// 	console.log("Someone in the class loves " + fruit);
-// });
-
-for (fruitIndex in favouriteFruits) {
-	console.log("Someone in the class loves " + favouriteFruits[fruitIndex]);
-}
-
-
-console.log(Array.isArray(favouriteFruits));
-
-
-// Array.prototype.sort()
-let sortedFavouriteFruits = favouriteFruits.sort();
-
-console.log(sortedFavouriteFruits);
-
-let someNewArray = new Array(10);
-
-someNewArray.push("mango");
-
-console.log(someNewArray);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Dictionary in Python is Object in JS
-
-let alex = {
-	name: "alex",
-	"codingLanguages": ["C#", "JavaScript", "HTML", "CSS"],
-	hobbies: [
-		{
-			"name": "gardening",
-			"skill": 10,
-		},
-		{
-			"name": "gaming",
-			"favouriteGames": ["pokemon", "halo"],
-			"skill": 9001,
-		}
-	],
-};
-
-alex.hobbies[1]["favouriteGames"][0] = "digimon";
-
-alex.hobbies[1].favouriteGames[0] = "palworld";
-
-console.log(alex);
-
-
-let weather = "rainy";
-
-switch (weather) {
-	case "sunny":
+	let main = {
+		name: "min"
+		"statemenshows "
+	}
+	
+	alex.hobbies[1]["favouriteGames"][0] = "digimon";
+	
+	alex.hobbies[1].favouriteGames[0] = "palworld";
+	
+	console.log(alex);
+	
+	
+	
+	/**
+	 * Statusof the weather, manually set.
+	 * @date 4/4/2024 - 8:59:52 PM 
+	 * @author BigFootDS
+	 */
+	let weather = "rainy";
+	
+	switch (weather) {
+		case "sunny":
+			console.log("Sunny weather is nice when it's not hot!");
+			break;
+		case "rainy":
+			console.log("Rain is nice when it's not torrential!");
+			break;
+		case "hail":
+			console.log("Man, that hail is wild");
+			// break;
+		case "storm":
+		case "snowy":
+		default: 
+			console.log("Crazy weather, am I right?");
+			break;
+	}
+	
+	
+	if (weather == "sunny") {
 		console.log("Sunny weather is nice when it's not hot!");
-		break;
-	case "rainy":
+	} else if (weather == "rainy") {
 		console.log("Rain is nice when it's not torrential!");
-		break;
-	case "hail":
-		console.log("Man, that hail is wild");
-		// break;
-	case "storm":
-	case "snowy":
-	default:
+	} else {
 		console.log("Crazy weather, am I right?");
-		break;
+	}
+
+	// array.forEach(element => {
+	// 	console.log("Today is a hot day");
+	// });
 }
 
+let pokemonTeam = [
+	"pikachu", 
+	"squirtle",
+	"charmander", 
+	"mew",
+]; 
 
-if (weather == "sunny") {
-	console.log("Sunny weather is nice when it's not hot!");
-} else if (weather == "rainy") {
-	console.log("Rain is nice when it's not torrential!");
-} else {
-	console.log("Crazy weather, am I right?");
+
+function logTheTeam () {
+	sortedTeam = pokemonTeam.sort();
+	
+	// sortedTeam.forEach((pokemon, arrayIndex) => {
+	// 	console.log(pokemon + " is at index of " + arrayIndex);
+	// });
+
+	// console.log(pokemonTeam.entries());
+	for (const [index, pokemon] of sortedTeam.entries()) {
+		console.log(`${pokemon} is at index of ${index}`);
+	}
+
+
+	let [firstPokemon, ...otherPokemon] = sortedTeam;
+
+	console.log(firstPokemon);
+	console.log(otherPokemon);
+
+	
 }
+
+logTheTeam();
 
 
 
